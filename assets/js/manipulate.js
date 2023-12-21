5963a9aa20891174bd5b3ccde38d922c829b0d6a
William <byweimar@gmail.com>
2023-12-20 20:39:14 -0500

    manipulate

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,33 @@
 function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
+    // const newTitle = document.title;
+    // console.log(document);
+    // const newTitle = document.getElementsByTagName('head title')
+    // newTitle.innerText = "Your name's Portfolio";
+    document.title = "Your Name's Portfolio"
 
-    // Your code here 
 }
 
 function changeHeader() {
     // Change the name in the h1 of the page to your name
+    const newHeader = document.querySelector('h1')
+    newHeader.innerText = "Your Name"
 
-    // Your code here 
 }
 
 function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
+    // const newBody = document.body.div
+    // console.log(newBody);
+    const newAbout = document.querySelector('.section:nth-of-type(2)')
+    newAbout.innerText = "Nothing interesting"
 
-    // Your code here 
+
+}
+
+export {
+    changeTitle,
+    changeHeader,
+    changeAboutMe
 }
